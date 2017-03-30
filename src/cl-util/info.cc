a09716415e577b21db0e33eb99bc5f68e9f576c4
Johannes Hidding <johannes.hidding@gmail.com>
2017-03-30 10:14:40 +0200

    ...

--- src/cl-util/info.cc
+++ src/cl-util/info.cc
@@ -13,7 +13,8 @@ void cl_tutorial::print_opencl_info(
     {
         auto const &platform = platform_list[i];
         console
-            .msg("== Nº ", i+1, " =================================================================")
+            .msg("== \033[1mPlatform Nº ", i+1, 
+			     "\033[m =========================================================")
             .push("  ")
             .msg("Name:      ", platform.getInfo<CL_PLATFORM_NAME>())
             .msg("Vendor:    ", platform.getInfo<CL_PLATFORM_VENDOR>());
@@ -22,21 +23,21 @@ void cl_tutorial::print_opencl_info(
         platform.getDevices(CL_DEVICE_TYPE_ALL, &device_list);
 
         console
-			.msg("  # devices: ", device_list.size())
+			.msg("# devices: ", device_list.size())
 			.msg();
 
         for (unsigned j = 0; j < device_list.size(); ++j)
         {
             auto const &device = device_list[j];
             console
-				.msg("-- Nº ", j+1, " ---------------------------------------------------------------")
+				.msg("-- \033[1mDevice Nº ", j+1,
+				     "\033[m ---------------------------------------------------------")
 				.push("  ")
                 .msg("Name:             ", device.getInfo<CL_DEVICE_NAME>())
                 .msg("Device version:   ", device.getInfo<CL_DEVICE_VERSION>())
 				.msg("Driver version:   ", device.getInfo<CL_DRIVER_VERSION>())
 				.msg("OpenCL version:   ", device.getInfo<CL_DEVICE_OPENCL_C_VERSION>())
 				.msg("# Compute units:  ", device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>())
-				.msg()
 				.pop();
         }
 
